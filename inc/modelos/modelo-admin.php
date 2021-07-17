4e6bdb1782ad7b3bd0779abf3fcab59328d57700
d3lm0nt <agudelmonte33@gmail.com>
2021-07-16 22:23:54 -0300

    creada función que envia datos del form de JS a PHP

---
+++ inc/modelos/modelo-admin.php
@@ -0,0 +1,9 @@
+<?php 
+
+// $arreglo = array(
+//     'respuesta' => 'desde Modelo!'
+// );
+
+die(json_encode($_POST)); //json es un formato de transporte entre php y js. 'die' es como un 'echo'
+
+?>
--- js/formulario.js
+++ js/formulario.js
@@ -11,8 +11,9 @@ function validarRegistro(e) {
     e.preventDefault();
 
     var usuario = document.querySelector('#usuario').value,
-        password = document.querySelector('#password').value;
-
+        password = document.querySelector('#password').value,
+        tipo = document.querySelector('#tipo').value;
+    //VALIDACIÓN DE CAMPOS
     if (usuario === '' || password === '') {
         Swal.fire({
             icon: 'error',
@@ -21,12 +22,30 @@ function validarRegistro(e) {
             footer: ''
         })
     } else {
-        Swal.fire({
-            icon: 'success',
-            title: '¡Perfecto!',
-            text: 'Completaste el registro con éxito',
+        //DATOS QUE SE ENVIAN AL SERVIDOR
+        var datos = new FormData(); // Estructura el llamado a ajax, darle llave y valor.
+        datos.append('usuario', usuario);
+        datos.append('password', password);
+        datos.append('accion', tipo);
 
-        })
+
+
+        //CREAR LLAMADO A AJAX
+        var xhr = new XMLHttpRequest();
+
+        //ABRIR LA CONEXIÓN
+        xhr.open('POST', 'inc/modelos/modelo-admin.php', true);
+
+        //RETORNO DE DATOS
+        xhr.onload = function() {
+            if (this.status === 200) {
+                console.log(JSON.parse(xhr.responseText)); //responseText trae el json del .php vinculado
+                //JSON.parse convierte la respuesta en un object
+            }
+        }
+
+        //ENVIAR LA PETICIÓN
+        xhr.send(datos);
     }
 
 }
